bf61fb58028f4658cef5c6daf8a6dbe4833ab762
Ibai Trueba <axiailus@gmail.com>
2019-05-07 13:54:06 +0200

    primer repositorio

---
+++ js/codigo.js
@@ -0,0 +1,7 @@
+let miNombre = prompt("Introduzca su nombre y apellido, por ejemplo Pepito Grillo.");
+
+if (miNombre == "Pepito Grillo") {
+  alert("Buenos días, veo que realmente eres " + miNombre + ".");
+} else {
+  alert("Buenos días " + miNombre + ".");
+}
