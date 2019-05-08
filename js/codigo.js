70cfb286e325d9d6bea7609bcb6678776939d108
Ibai Trueba <axiailus@gmail.com>
2019-05-08 09:56:32 +0200

    primera mejora

--- js/codigo.js
+++ js/codigo.js
@@ -1,7 +1,9 @@
 let miNombre = prompt("Introduzca su nombre y apellido, por ejemplo Pepito Grillo.");
-
-if (miNombre == "Pepito Grillo") {
+  // Mostrará la alerta y despues lo mostrara en pantalla.
+if (miNombre === "Pepito Grillo") {
   alert("Buenos días, veo que realmente eres " + miNombre + ".");
+  document.getElementById("nombre").innerHTML = ("Buenos días, veo que realmente eres " + miNombre + ".");
 } else {
   alert("Buenos días " + miNombre + ".");
+  document.getElementById("nombre").innerHTML = ("Buenos días " + miNombre + ".");
 }
